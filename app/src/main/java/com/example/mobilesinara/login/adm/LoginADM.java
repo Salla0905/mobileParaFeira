83c8cf8682fdf2c4350e0ffa902bae8d11b1c899
Salla0905 <bruna.salla@germinare.org.br>
2025-11-04 08:52:49 -0300

    subindo projeto local

--- app/src/main/java/com/example/mobilesinara/login/adm/LoginADM.java
+++ app/src/main/java/com/example/mobilesinara/login/adm/LoginADM.java
@@ -37,7 +37,6 @@ public class LoginADM extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         EdgeToEdge.enable(this);
         setContentView(R.layout.activity_login_adm);
-
         ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main), (v, insets) -> {
             Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
